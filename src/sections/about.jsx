c9c1f7ab57f2ac36d1dca2b01dddbb10d318744b
sickopam <sickopam@gmail.com>
2023-04-10 10:54:41 +0700

    cleared about tw

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -4,13 +4,13 @@ import Me from '../images/me.webp'
 export default function About({isVisibleAbout, setShowAbout, fadePropsAbout}) {
 
   return (
-    <div {...fadePropsAbout} id='modal' className='space-y-12 text-xl overflow-y-scroll text-center md:text-justify'>
-        <div className='lg:flex justify-between space-y-5 lg:space-y-0 lg:space-x-5'>
-            <img id='image' className='w-full lg:w-[40%]' src={Me} alt='me'></img>
-            <div className='flex flex-col space-y-3'>
+    <div {...fadePropsAbout} id='modal'>
+        <div id='me-n-details'>
+            <img id='image' src={Me} alt='me'></img>
+            <div id='detail-container'>
                 {details.map((e, i) => (
-                    <div key={i} className='flex-wrap md:inline-flex md:space-x-1'>
-                        <h2 className='font-semibold'>{e.title}</h2>
+                    <div key={i} id='details'>
+                        <h2 id='title'>{e.title}</h2>
                         <h3>{e.sub}</h3>
                     </div>
                 ))}
@@ -21,7 +21,7 @@ export default function About({isVisibleAbout, setShowAbout, fadePropsAbout}) {
         </div>
 
         <div>
-            <p className=''>a student driven by motivation and passion to work. i am able to adapt to new working environments, as well as get along with co-workers in a brief period of time. i have active hobbies that accompany me throughout times, thus, laziness was never an option for me, especially when it comes to working professionally with other people or comrades.</p>
+            <p>a student driven by motivation and passion to work. i am able to adapt to new working environments, as well as get along with co-workers in a brief period of time. i have active hobbies that accompany me throughout times, thus, laziness was never an option for me, especially when it comes to working professionally with other people or comrades.</p>
         </div>
 
         <div className='flex justify-center block lg:hidden'>
